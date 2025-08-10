60b25a3802ea4ce1cd355fb0fdb3aacccd55085a
Csillag61 <csillagok@gmail.com>
2025-08-10 18:37:52 -0400

    clean

--- taxi/views.py
+++ taxi/views.py
@@ -28,7 +28,7 @@ class CarListView(ListView):
 
 class CarDetailView(DetailView):
     model = Car
-    queryset = Car.objects.prefetch_related("drivers")
+    queryset = Car.objects.select_related("manufacturer").prefetch_related("drivers")
 
 
 class DriverListView(ListView):
